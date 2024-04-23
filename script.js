e6fa47e5ad812fc68dd2ffbe56d042be7a6a1de8
omidzarghami <omidzarghami@gmail.com>
2024-04-23 20:49:21 +0330

    add top button

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
 //     document.body.classList.add('dark-mode');
 // }
 
-document.querySelector('.hamburger-menu').addEventListener('click', function() {
+document.querySelector('.hamburger-menu').addEventListener('click', function () {
     document.querySelector('.menu').classList.toggle('active');
 });
 
@@ -25,3 +25,23 @@ document.querySelector('.hamburger-menu').addEventListener('click', function() {
 // document.getElementById('enlarged-image').addEventListener('click', function() {
 //     this.style.display = 'none';
 // });
+
+
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
